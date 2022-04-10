88ca362eeb8c82812eb77cae6321f125c183bfe0
Mae Moninghoff <jcmoninghoff@gmail.com>
2022-04-09 21:24:22 -0400

    Remove Main

--- ttvController.py
+++ ttvController.py
@@ -8,41 +8,6 @@ from config import *
 import osrsController as osrs
 
 
-def main(): 
-
-    server = 'irc.chat.twitch.tv'
-    port = 6667
-    nickname = 'newtwitchplaysosrs'
-
-    sock = socket.socket()
-
-    sock.connect((server, port))
-    sock.send(f"PASS {token}\n".encode('utf-8'))
-    sock.send(f"NICK {nickname}\n".encode('utf-8'))
-    sock.send(f"JOIN {channel}\n".encode('utf-8'))
-
-    print("Started")
-
-    ttvController = ttvController()
-
-    while True:
-        resp = sock.recv(2048).decode('utf-8')
-
-        if resp.startswith('PING'):
-            sock.send("PONG\n".encode('utf-8'))
-        
-        elif len(resp) > 0:
-            print(resp)
-            try:
-                ttvController.readChat(resp)
-            except:
-                print("An exception occurred")
-        
-        time.sleep(0.6)
-
-
-
-
 class winSize:
     def __init__(self):
         self.xMax = 760
