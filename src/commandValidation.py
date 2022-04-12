fa4066617ce1551fde25734d493f9231629208f5
Mae Moninghoff <jcmoninghoff@gmail.com>
2022-04-12 01:41:48 -0400

    organized by interaction method, light comments

--- src/commandValidation.py
+++ src/commandValidation.py
@@ -2,12 +2,15 @@ import re
 import osrsController as osrs
 import utility
 
+
+## Organized these by general area they are working with. Worth considering putting these into objects for mouse, keyboard, main, inventory, hud, inventoryhud
+
 class ValidationController:
     def __init__(self, osrs, win):
         self.osrs = osrs
         self.win = win
     
-
+    # What this does
     def isValidInput(self, line):
         public_method_names = [method for method in dir(self) if callable(getattr(self, method)) if not method.startswith('_') if not method.startswith("isValidInput")]  # 'private' methods start from _
         for method in public_method_names:
@@ -16,93 +19,51 @@ class ValidationController:
         return False
         
 
+    ######################### MOUSE VALIDATION #########################
 
-    def validInvLClick(self, line):
-        if re.match("^([w-z][1-9][0-3]?)( +.*)?$", line):
-            num = utility.getFirstNumber(line)
-            return self.osrs.checkInvCoord(line[0], num)
-        return False
-    
-    def validInvRClick(self, line):
-        if re.match("^(r[w-z][1-9])( +.*)?$", line):
-            num = utility.getFirstNumber(line)
-            return self.osrs.checkInvCoord(line[1], num)
-        return False
-    
-    def validDrag(self, line):
-        if re.match("^drag ?([w-z][1-9]) ?([w-z][1-9])( +.*)?$", line):
-            lineWords = line.split(' ')
-            firstBar = lineWords[1]
-            secondBar = lineWords[2]
-            num1 = utility.getFirstNumber(firstBar)
-            num2 = utility.getFirstNumber(secondBar)
-            return self.osrs.checkInvCoord(firstBar[0], num1) and self.osrs.checkInvCoord(secondBar[0], num2)
-        return False
-    
-    def validMainLClick(self, line):
-        if re.match("^([a-t][1-9][0-3]?)( +.*)?$", line):
-            letter = line[0]
-            num = utility.getFirstNumber(line)
-            return self.osrs.checkMainCoord(letter, num)
-        return False
-    
-    def validMainRClick(self, line):
-        if re.match("^(r[a-t][1-9][0-3]?)( +.*)?$", line):
-            letter = line[1]
-            num = utility.getFirstNumber(line)
-            return self.osrs.checkMainCoord(letter, num)
-        return False
-    
-    def validDrop(self, line):
-        #perform left click on coordinates given
-        if re.match("^(drop [w-z][1-7])( +.*)?$", line): 
-            coords = line.split(' ')[1].strip()
-            num = utility.getFirstNumber(coords)
-            return self.osrs.checkInvCoord(coords[0], num)
-        return False
-
-    def validBankDeposit(self,line):
-        if re.match("^((bank ))((inv|inventory|equipment|equip)( +.*)?)?$",line):
-            return True
-        return False
-
-    def validBankQuantity(self,line):
-        if re.match("^((bank ))((q1|q5|q10|qx|qall)( +.*)?)+",line):
-            return True
-        return False
-    
-    def validMapMove(self, line):
-        if re.match("^((map)|(mm)|(m) ((tr)|(ur)|(br)|(bl)|(ul)|(tl)|(bottom)|(top)|(bot)|(down)|(left)|(right)|(t)|(b)|(d)|(l)|(r)))( +.*)?$", line): 
-            return True
-        return False
-    
-    def validEscape(self, line):
-        return line in ["escape", "cancel", "quit", "exit", "esc"]
-    
-    def validQuickUse(self, line):
-        return "qu"==line or line.startswith("qu ")
-    
     def validClick(self, line):
         return line in ["c", "lc", "click", "left click", "lclick"]
-    
+
+
     def validRClick(self, line):
         return line in ["r", "rc", "right click", "r click", "rclick"]
+
+
+    def validMouseMoveTo(self, line): ## I think we should change this to be "mouse x y"
+        if line.startswith("move mouse to"):
+            dur = line.split(' ')
+            return len(dur) == 5 and (int(dur[3]) <= self.win.xMax and int(dur[4])*-1 <= self.win.yMax)
+        return False
     
-    def validStop(self, line):
-        return line=="stop"
+    def validMouseMove(self, line): ## and change this to be "mouserel x y" or smth, not as sold on this. we could also do like "mouse [dir] [num]"
+        if line.startswith("move mouse "):
+            dur = line.split(' ')
+            return len(dur) == 4 and (int(dur[2]) <= self.win.xMax and int(dur[3])*-1 <= self.win.yMax)
+        return False
     
-    def validResetCam(self, line):
-        return line in ["reset", "reset camera", "compass"]
+    def validCenterMouse(self, line):
+        return line.startswith("center mouse")
     
-    def validRun(self, line):
-        return line in ["run", "walk", "sprint"]
+    def validMenu(self, line):
+        if re.match("^(m|menu) [0-9]( +.*)?$", line):
+            return True
+        return False
     
-    def validSpecialAttack(self, line):
-        return line in line in ["sa", "special", "special attack"]
+    def validZoom(self, line):
+        return line=="zoom in" or line=="zoom out"
     
-    def validPrayer(self, line):
-        return line in ["pray", "prayer"]
+    def validScroll(self, line):
+        return line=="scroll up"or line=="scroll down"
+
+    #####################################################################
     
+
+
+    ######################### KEYPRESS VALIDATIONS #########################
+
+    def validEscape(self, line):
+        return line in ["escape", "cancel", "quit", "exit", "esc"]
+
     def validSay(self, line):
         if(re.match("^say: ((?!::).)*$", line)):
             return True
@@ -131,37 +92,124 @@ class ValidationController:
             if dir in ['qtr','qtl']:
                 return True
         return False
-    
-    def validMouseMoveTo(self, line):
-        if line.startswith("move mouse to"):
-            dur = line.split(' ')
-            return len(dur) == 5 and (int(dur[3]) <= self.win.xMax and int(dur[4])*-1 <= self.win.yMax)
-        return False
-    
-    def validMouseMove(self, line):
-        if line.startswith("move mouse "):
-            dur = line.split(' ')
-            return len(dur) == 4 and (int(dur[2]) <= self.win.xMax and int(dur[3])*-1 <= self.win.yMax)
-        return False
-    
-    def validCenterMouse(self, line):
-        return line.startswith("center mouse")
-    
-    def validMenu(self, line):
-        if re.match("^(m|menu) [0-9]( +.*)?$", line):
-            return True
-        return False
-    
-    def validZoom(self, line):
-        return line=="zoom in" or line=="zoom out"
-    
-    def validScroll(self, line):
-        return line=="scroll up"or line=="scroll down"
-    
+
     def validSpace(self, line):
         if re.match("^((spacebar)|(space))( [0-9]+)?( +.*)?$", line):
             return True
         return False
+
+    def validNumerical(self, line):
+        return line.isnumeric()
+
+    #########################################################################
+
+
+
+    ######################### MAIN WINDOW VALIDATION #########################
+
+    def validMainLClick(self, line):
+        if re.match("^([a-t][1-9][0-3]?)( +.*)?$", line):
+            letter = line[0]
+            num = utility.getFirstNumber(line)
+            return self.osrs.main.checkCoord(letter, num)
+        return False
+    
+    def validMainRClick(self, line):
+        if re.match("^(r[a-t][1-9][0-3]?)( +.*)?$", line):
+            letter = line[1]
+            num = utility.getFirstNumber(line)
+            return self.osrs.main.checkCoord(letter, num)
+        return False
+
+    def validStop(self, line):
+        return line=="stop"
+    
+
+    def validBankDeposit(self,line):
+        if re.match("^((bank ))((inv|inventory|equipment|equip)( +.*)?)?$",line):
+            return True
+        return False
+
+    def validBankQuantity(self,line):
+        if re.match("^((bank ))((q1|q5|q10|qx|qall)( +.*)?)+",line):
+            return True
+        return False
+
+    def validDir(self,line):
+        if re.match("^((left)|(right)|(down)|(up)|(u)|(d)|(l)|(r))(( [1-7])( +[^1-7].*)?)?$", line):
+           return True
+        return False 
+
+    ###########################################################################
+
+
+
+    ######################### INVENTORY VALIDATION #########################
+
+    def validInvLClick(self, line):
+        if re.match("^([w-z][1-9][0-3]?)( +.*)?$", line):
+            num = utility.getFirstNumber(line)
+            return self.osrs.inv.checkCoord(line[0], num)
+        return False
+
+
+    def validInvRClick(self, line):
+        if re.match("^(r[w-z][1-9])( +.*)?$", line):
+            num = utility.getFirstNumber(line)
+            return self.osrs.inv.checkCoord(line[1], num)
+        return False
+
+
+    def validDrag(self, line):
+        if re.match("^drag ?([w-z][1-9]) ?([w-z][1-9])( +.*)?$", line):
+            lineWords = line.split(' ')
+            firstBar = lineWords[1]
+            secondBar = lineWords[2]
+            num1 = utility.getFirstNumber(firstBar)
+            num2 = utility.getFirstNumber(secondBar)
+            return self.osrs.inv.checkCoord(firstBar[0], num1) and self.osrs.inv.checkCoord(secondBar[0], num2)
+        return False
+    
+    
+    def validDrop(self, line):
+        #perform left click on coordinates given
+        if re.match("^(drop [w-z][1-7])( +.*)?$", line): 
+            coords = line.split(' ')[1].strip()
+            num = utility.getFirstNumber(coords)
+            return self.osrs.inv.checkCoord(coords[0], num)
+        return False
+
+    def validQuickUse(self, line):
+        return "qu"==line or line.startswith("qu ")
+
+    ########################################################################
+
+
+
+    ######################### HUD VALIDATIONS #########################
+
+    def validMapMove(self, line):
+        if re.match("^((map)|(mm)|(m) ((tr)|(ur)|(br)|(bl)|(ul)|(tl)|(bottom)|(top)|(bot)|(down)|(left)|(right)|(t)|(b)|(d)|(l)|(r)))( +.*)?$", line): 
+            return True
+        return False
+    
+    def validResetCam(self, line):
+        return line in ["reset", "reset camera", "compass"]
+    
+    def validRun(self, line):
+        return line in ["run", "walk", "sprint"]
+    
+    def validSpecialAttack(self, line):
+        return line in line in ["sa", "special", "special attack"]
+    
+    def validPrayer(self, line):
+        return line in ["pray", "prayer"]
+    
+    ####################################################################
+
+    
+
+    ######################### INVENTORY HUD BUTTON VALIDATION #########################
     
     def validCombat(self, line):
         return  line == "combat" or line == "style" or line == "attack style"
@@ -199,12 +247,8 @@ class ValidationController:
     def validMusic(self, line):
         return line == "music" or line == "song" or line =="songs"
     
-    def validNumerical(self, line):
-        return line.isnumeric()
+    ######################################################################################
 
-    def validDir(self,line):
-        if re.match("^((left)|(right)|(down)|(up)|(u)|(d)|(l)|(r))(( [1-7])( +[^1-7].*)?)?$", line):
-           return True
-        return False 
+
     
     
