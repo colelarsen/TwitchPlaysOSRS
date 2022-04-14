e78e53c98c652d29e4e6c17fa65dea3cc1e9a50f
colelarsen <larsenca@appstate.edu>
2022-04-13 20:19:29 -0400

    added text on screen queue!

--- src/discordReader.py
+++ src/discordReader.py
@@ -2,6 +2,7 @@ import requests
 import discord
 from discord.ext import commands
 from discord import Client
+import fileHandler
 
 class DiscordBot(discord.Client):
     def __init__(self, commandList, messageQueue, *args, **kwargs):
@@ -23,7 +24,7 @@ class DiscordBot(discord.Client):
         #TODO allows logout, bankpin to only come from 963799043170009098 (trusted discord plays channel)
         if (message.channel.id == 963799439020007514 or message.channel.id == 963799043170009098):
             self.commandList.append(message.content)
-            self.messageQueue.put("Discord: " + message.author + ": " + message.content)
+            fileHandler.addLineToQueue("Discord: " + message.author.name + ": " + message.content, self.messageQueue)
 
 
         
--- src/fileHandler.py
+++ src/fileHandler.py
@@ -1,8 +1,20 @@
 import queue
+
+
+
 def writeQueueToFile(filePath, queue):
     file1 = open(filePath,"w")
+    textToWrite = ""
 
     for line in queue.queue:
-        file1.write(line + "\n")
-    
+        textToWrite = textToWrite + line + "\n"
+        
+    file1.write(textToWrite)
     file1.close()
+
+def addLineToQueue(line, queue):
+    if len(queue.queue) == 25:
+        queue.get(0)
+    
+    queue.put(line)
+
