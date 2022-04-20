d342735182ac0cb2c0fc753494a6101ab7cabd23
Mae Moninghoff <jcmoninghoff@gmail.com>
2022-04-19 20:47:16 -0400

    added getSecondNumber

--- src/utility.py
+++ src/utility.py
@@ -4,3 +4,10 @@ def getFirstNumber(line):
     if number:
         number = int(line[number.start():number.end()])
     return number
+
+def getSecondNumber(line):
+    number = re.search(r"[\d]+ [\d]+", line)
+    if number:
+        nums = line[number.start():number.end()]
+        number = int(nums.split(' ')[1])
+    return number
